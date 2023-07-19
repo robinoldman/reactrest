df452f3d93092dac1696ca77d019ed7d9fac6f50
robinoldman <robin.c.oldman@gmail.com>
2023-07-19 14:15:21 +0000

    initial commit

---
+++ reactrest/reactrest/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for reactrest project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'reactrest.settings')
+
+application = get_asgi_application()
